489fe66a090a36293c0589827694d3bb51c4203a
SomtochiUmeh <74836691+SomtochiUmeh@users.noreply.github.com>
2021-10-15 20:19:55 -0700

    Somto (#11)
    * My solution to 1
    
    * My solution to question 1
    
    Co-authored-by: Somtochi Umeh <somto@Somtochis-MacBook-Air.local>

---
+++ contributions/1-SomtochiUmeh.py
@@ -0,0 +1,25 @@
+# ask user to input year they'd like to check
+year = int(input("Which year do you want to check? "))
+
+# function to check if a year is a leap year
+def leapyear_check(year):
+    # if year divisible by 4
+    if year % 4 == 0:
+        # and divisible by 100
+        if year % 100 != 0:
+            # then it's a leap year
+            print('Leap year.')
+        # or divisible by hundred and four hundred
+        elif year % 100 == 0 and year % 400 == 0:
+            # then it's a leap year
+            print('Leap year.')
+        else:
+            # else, not a leap year
+            print('Not leap year.')
+    
+    # if year not divisible by 4
+    else:
+        # then it's not a leap year
+        print('Not leap year.')
+
+leapyear_check(year)
